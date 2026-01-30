b4086b4b20926ef1ac5c66c8cdaad26420ac12f6
Patrick Jakobsen <patrick.jakobsen@icloud.com>
2026-01-30 22:58:08 +0100

    feat: Add a welcome message and `Smile` icon to the landing page, and update the login button's styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
-import { LogIn } from "lucide-react";
+import { LogIn, Smile } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
 const Page = () => {
   return (
-    <div className="flex min-h-svh items-center justify-center">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-4">
+      <Smile className="size-20 animate-bounce fill-orange-400/10 text-orange-400" />
+      <h1 className="text-4xl font-bold">Welcome to the Booking App</h1>
       <Link href="/sign-in">
-        <Button>
+        <Button className="cursor-pointer px-90 py-40 text-2xl">
           <LogIn /> Login
         </Button>
       </Link>
